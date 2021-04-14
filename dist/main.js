b67d20e691a1da3fd308cd22db4721b2caef8638
kenny <kenny@192.168.0.136>
2021-04-14 06:24:22 -0500

    htmlwebpackplugin and babel added

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,116 @@
-console.log("Hello Kenny");
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/classCallCheck.js":
+/*!*******************************************************************!*\
+  !*** ./node_modules/@babel/runtime/helpers/esm/classCallCheck.js ***!
+  \*******************************************************************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ _classCallCheck)\n/* harmony export */ });\nfunction _classCallCheck(instance, Constructor) {\n  if (!(instance instanceof Constructor)) {\n    throw new TypeError(\"Cannot call a class as a function\");\n  }\n}\n\n//# sourceURL=webpack://webtraining-css/./node_modules/@babel/runtime/helpers/esm/classCallCheck.js?");
+
+/***/ }),
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/createClass.js":
+/*!****************************************************************!*\
+  !*** ./node_modules/@babel/runtime/helpers/esm/createClass.js ***!
+  \****************************************************************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ _createClass)\n/* harmony export */ });\nfunction _defineProperties(target, props) {\n  for (var i = 0; i < props.length; i++) {\n    var descriptor = props[i];\n    descriptor.enumerable = descriptor.enumerable || false;\n    descriptor.configurable = true;\n    if (\"value\" in descriptor) descriptor.writable = true;\n    Object.defineProperty(target, descriptor.key, descriptor);\n  }\n}\n\nfunction _createClass(Constructor, protoProps, staticProps) {\n  if (protoProps) _defineProperties(Constructor.prototype, protoProps);\n  if (staticProps) _defineProperties(Constructor, staticProps);\n  return Constructor;\n}\n\n//# sourceURL=webpack://webtraining-css/./node_modules/@babel/runtime/helpers/esm/createClass.js?");
+
+/***/ }),
+
+/***/ "./src/app.js":
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _js_alert_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./js/alert.js */ \"./src/js/alert.js\");\n\n$(document).ready(function () {\n  console.log(\"document ready..\");\n  $('#btn-sidebar-collapser').on('click', function () {\n    // $('.overlay').removeClass('active');\n    $('.sidebar').addClass('active');\n    $('#body-content').addClass('active'); // new AlertWeb().showMessage('my own alert');\n  });\n  $('#btn-sidebar-toogle').on('click', function () {\n    // $('#sidebar').toggleClass('active');\n    $('.sidebar').toggleClass('active');\n    $('#body-content').toggleClass('active');\n    console.log(\"click\");\n    new _js_alert_js__WEBPACK_IMPORTED_MODULE_0__.AlertWeb().print(\"very good\");\n  });\n});\n\n//# sourceURL=webpack://webtraining-css/./src/app.js?");
+
+/***/ }),
+
+/***/ "./src/js/alert.js":
+/*!*************************!*\
+  !*** ./src/js/alert.js ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"AlertWeb\": () => (/* binding */ AlertWeb)\n/* harmony export */ });\n/* harmony import */ var _babel_runtime_helpers_classCallCheck__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/helpers/classCallCheck */ \"./node_modules/@babel/runtime/helpers/esm/classCallCheck.js\");\n/* harmony import */ var _babel_runtime_helpers_createClass__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @babel/runtime/helpers/createClass */ \"./node_modules/@babel/runtime/helpers/esm/createClass.js\");\n\n\n\nvar AlertWeb = /*#__PURE__*/function () {\n  function AlertWeb() {\n    (0,_babel_runtime_helpers_classCallCheck__WEBPACK_IMPORTED_MODULE_0__.default)(this, AlertWeb);\n  }\n\n  (0,_babel_runtime_helpers_createClass__WEBPACK_IMPORTED_MODULE_1__.default)(AlertWeb, [{\n    key: \"showMessage\",\n    value: function showMessage(data) {\n      // console.log(\"message\")\n      alert(data, 'title');\n    }\n  }, {\n    key: \"print\",\n    value: function print(message) {\n      console.log(message);\n    }\n  }]);\n\n  return AlertWeb;\n}();\n\n\n\n//# sourceURL=webpack://webtraining-css/./src/js/alert.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/app.js");
+/******/ 	
+/******/ })()
+;
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,39 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  // Entry nos permite decir el punto de entrada de nuestra aplicación
-  entry: "./src/index.js",
-  // Output nos permite decir hacia dónde va enviar lo que va a preparar webpacks
-  output: {
-    // path es donde estará la carpeta donde se guardará los archivos
-    // Con path.resolve podemos decir dónde va estar la carpeta y la ubicación del mismo
-    path: path.resolve(__dirname, "dist"),
-    // filename le pone el nombre al archivo final
-    filename: "main.js"
-  },
-  resolve: {
-    // Aqui ponemos las extensiones que tendremos en nuestro proyecto para webpack los lea
-    extensions: [".js"]
-  },
+    // Entry nos permite decir el punto de entrada de nuestra aplicación
+    entry: "./src/app.js",
+    // Output nos permite decir hacia dónde va enviar lo que va a preparar webpacks
+    output: {
+        // path es donde estará la carpeta donde se guardará los archivos
+        // Con path.resolve podemos decir dónde va estar la carpeta y la ubicación del mismo
+        path: path.resolve(__dirname, "dist"),
+        // filename le pone el nombre al archivo final
+        filename: "main.js"
+    },
+    resolve: {
+        // Aqui ponemos las extensiones que tendremos en nuestro proyecto para webpack los lea
+        extensions: [".js"]
+    },
+    module: {
+        // REGLAS PARA TRABAJAR CON WEBPACK
+        rules: [{
+            // Test declara que extensión de archivos aplicara el loader
+            test: /\.m?js$/,
+            // Use es un arreglo u objeto donde dices que loader aplicaras
+            use: {
+                loader: "babel-loader"
+            },
+            // Exclude permite omitir archivos o carpetas especificas
+            exclude: /node_modules/
+        }]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ // CONFIGURACIÓN DEL PLUGIN
+          inject: true, // INYECTA EL BUNDLE AL TEMPLATE HTML   default true ... true || 'head' || 'body' || false
+          template: './public/index.html', // LA RUTA AL TEMPLATE HTML
+          filename: './index.html' // NOMBRE FINAL DEL ARCHIVO
+      })
+  ]
 }
